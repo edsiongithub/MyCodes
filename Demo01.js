e4832f680c5ace992abe09b6f9d4049d1a4e2508
edsiongithub <gxwang8904@gmail.com>
2018-10-14 11:33:56 +0800

    add a file and modify a file

---
+++ Demo01.js
@@ -0,0 +1,3 @@
+function Demo(){
+    return new Date();
+}
--- Test.js
+++ Test.js
@@ -1,4 +1,6 @@
 function test(){
     console.log('this is code from vs code with git');
     alert('files changed....');
+    //modify this file again
+    return new Date();
 }
